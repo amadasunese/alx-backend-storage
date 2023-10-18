f00abdac249eb7c8e0eda38fa6bcbca3a847ac72
Ese Amadasun <amadasunese@gmail.com>
2023-10-18 21:43:03 +0100

    updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,25 +11,22 @@ cache = TTLCache(maxsize=100, ttl=10)
 
 
 def get_page(url: str) -> str:
-    # Check if the URL is in the cache
+    """
+    Return the cached content
+    """
     if url in cache:
-        # If it's in the cache, return the cached content
         return cache[url]
 
-    # If not in the cache, make an HTTP request
     response = requests.get(url)
 
     if response.status_code == 200:
-        # If the request was successful, store the content in the cache
         cache[url] = response.text
     else:
-        # Handle the case where the request was not successful
         response.raise_for_status()
 
     return response.text
 
 
-# Function to track the number of times a URL was accessed
 def track_access_count(url):
     count_key = f"count:{url}"
     access_count = cache.get(count_key, 0)
@@ -37,7 +34,6 @@ def track_access_count(url):
     cache[count_key] = access_count
 
 
-# Decorator for tracking access count
 def track_access(func):
     @wraps(func)
     def wrapper(url):
@@ -46,10 +42,9 @@ def track_access(func):
     return wrapper
 
 
-# Apply the decorator to the get_page function
 get_page = track_access(get_page)
 
-# Example usage
+
 if __name__ == "__main__":
     url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.example.com"
 
